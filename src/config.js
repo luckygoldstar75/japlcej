9a315621d8a81153e7acf6076467587f1df9fd20
Yo Nel <yonel@yopmail.com>
2019-07-03 12:30:52 +0200

    config server adapted + backlog added

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
 import './config-login.js'
 
-export const japlcejAPI = 'http://localhost:4000/';
+ 
+export const japlcejAPI = 'http://localhost:4000/'||'https://japlcej.herokuapp.com';
 export const routesURLs = {
  GUESS_CHARACTER : 'guessCharacter',
- IS_LOGGEDIN : 'login',
+ IS_LOGGEDIN : 'login',	
  LOGIN : 'login',
  LOGOUT  :'logout',
  USER_RANK_INFO : 'rank'};
